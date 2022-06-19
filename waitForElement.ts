072ea3e8933c5a2a53155913c564ebd70e4db0c3
hwoodall30 <75491683+hwoodall30@users.noreply.github.com>
2022-06-18 21:52:08 -0400

    Update waitForElement.ts

--- waitForElement.ts
+++ waitForElement.ts
@@ -1,20 +1,19 @@
-async function waitForElement(selector: any, timeout: number = 5000, interval: number = 0): Promise<HTMLElement | Boolean> {
-	// Request Animation Frame Async
-	function rafAsync(): Promise<any> {
-		return new Promise(resolve => requestAnimationFrame(resolve));
-	}
-	// Sleep Async Function
-	const sleep = () => new Promise(r => setTimeout(r, interval));
-	//Get start time for timeout
-	const start = Date.now();
-	// Loop until element is found or timeout is reached
-	while (document.querySelector(selector) === null) {
-		if (Date.now() - start >= timeout) {
-			console.error(`Element Check For Selector ${selector} Timed Out`);
-			return false;
+	async function waitForElement(selector: any, timeout: number = 5000, interval: number = 0): Promise<HTMLElement | Boolean> {
+		// Request Animation Frame Async
+		const rafAsync = (): Promise<any> => new Promise(resolve => requestAnimationFrame(resolve));
+		// Sleep Async Function
+		const sleep = () => new Promise(r => setTimeout(r, interval));
+		//Get start time for timeout
+		const start = Date.now();
+		// Loop until element is found or timeout is reached
+		while (!document.querySelector(selector)) {
+			if (Date.now() - start >= timeout) {
+				console.error(`Element Check For Selector ${selector} Timed Out`);
+				return false;
+			}
+			console.log('waiting for element');
+			if (interval) await sleep();
+			await rafAsync();
 		}
-		if (interval) await sleep();
-		await rafAsync();
+		return document.querySelector(selector)!;
 	}
-	return document.querySelector(selector)!;
-}
